1955867f710370555ac075540dea46031db519b2
RSmetanyuk <rsmetanyuk@gmail.com>
2018-09-14 00:50:32 +0300

    sumElements not finihed

--- src/sumElements/index.js
+++ src/sumElements/index.js
@@ -11,7 +11,13 @@
  * sumElements([Infinity, NaN, 1]); // 1
  */
 const sumElements = arr => {
-  /* your logic here...*/
+    var sum = 0;
+    for (var i = 0; i < arr.length; i++) {
+      if (!isNaN(arr[i]) && arr[i] !== Infinity) {
+      	sum += parseInt(arr[i]);
+      }      
+    };
+    return sum;
 };
 
 export default sumElements;
